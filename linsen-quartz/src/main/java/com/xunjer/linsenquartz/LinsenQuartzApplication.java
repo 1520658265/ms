b79d3d9df98e0fb3f1d3f5479e8b2a429c1d8bda
linsen <1520658265@qq.com>
2020-08-17 19:12:26 +0800

    增加

---
+++ linsen-quartz/src/main/java/com/xunjer/linsenquartz/LinsenQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.xunjer.linsenquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinsenQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinsenQuartzApplication.class, args);
+    }
+
+}
